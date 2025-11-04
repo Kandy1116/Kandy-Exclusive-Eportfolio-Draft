c823dc89b062847e58a9b8794c287106db984bd2
Kandy1116 <venus.kandy@gmail.com>
2025-11-04 13:59:24 -0600

    update work

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ function contact(event) {
     loading.classList += " modal__overlay--visible" 
     setTimeout(() => {
         loading.classList.remove("modal__overlay--visible" )
-        sucess.classList += " modal__overlay--visible";
+        success.classList += " modal__overlay--visible";
         console.log('it worked1')
     }, 1000);
 }
